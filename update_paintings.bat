33cefc8e17b1bd62fe8627d41f5b054e3263a6d9
Sean Reilly <qwv4851@gmail.com>
2025-02-04 10:40:01 -0800

    Update batch file

--- update_paintings.bat
+++ update_paintings.bat
@@ -1,9 +1,8 @@
-
 :: Check the status of the repo
 git status
 
-:: Stage the changes to paintings.txt
-git add paintings.txt
+:: Stage the changes
+git add paintings.json
 git add art/*
 
 :: Commit the changes with a message
@@ -11,9 +10,6 @@ git commit -m "Update paintings"
 
 :: Push the changes to the remote repository
 git push origin main
-
-:: Optionally, you can specify a different branch if needed
-:: git push origin your-branch-name
 
 echo Changes have been pushed successfully!
 pause
