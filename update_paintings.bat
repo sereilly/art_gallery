789ea24a553c3d8cc2b8617cd41e635be748ec61
Sean Reilly <qwv4851@gmail.com>
2025-02-04 10:20:47 -0800

    Update paintings.txt

---
+++ update_paintings.bat
@@ -0,0 +1,19 @@
+
+:: Check the status of the repo
+git status
+
+:: Stage the changes to paintings.txt
+git add paintings.txt
+git add art/*
+
+:: Commit the changes with a message
+git commit -m "Update paintings.txt"
+
+:: Push the changes to the remote repository
+git push origin main
+
+:: Optionally, you can specify a different branch if needed
+:: git push origin your-branch-name
+
+echo Changes have been pushed successfully!
+pause
